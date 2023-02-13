453f7daa421b0c5474767b6d376f13f28cf31c17
Duck1s <32653@ma-web.nl>
2023-02-13 19:29:24 +0100

    setup(clean): empty files with example data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
+// Imports
 import { type AppType } from "next/dist/shared/lib/utils";
-
 import "../styles/globals.css";
 
+// Functions
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
 
+// Exports
 export default MyApp;
