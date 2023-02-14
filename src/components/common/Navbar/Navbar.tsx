8579cddcbadefe17a4347b357df7bf720afe2fdf
Duck1s <32653@ma-web.nl>
2023-02-14 20:52:05 +0100

    feat(information): add navbar states for different pages and fix button styles

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -5,13 +5,17 @@ import Button from "../Button/Button";
 // Functions
 const Navbar = () => {
   const router = useRouter();
+  const { pathname } = router;
 
   return (
     <nav className="flex items-center justify-between">
-      <h3>Stageblog CanvasHeroes</h3>
-      <Button size="small" onClick={() => router.push("/information")}>
-        Informatie
-      </Button>
+      {pathname === "/" && <h3>Stageblog CanvasHeroes</h3>}
+      {pathname === "/" && (
+        <Button onClick={() => router.push("/information")}>Informatie</Button>
+      )}
+      {pathname === "/information" && (
+        <Button onClick={() => router.push("/")}>&#60;&minus; Terug</Button>
+      )}
     </nav>
   );
 };
