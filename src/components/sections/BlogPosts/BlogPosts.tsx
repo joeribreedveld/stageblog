5aa77219ea69eee4578d5c9d023d3f543704ccdb
Duck1s <32653@ma-web.nl>
2023-02-14 22:07:05 +0100

    feat(blogposts): change spacing between elements and sizes for responsive

--- src/components/sections/BlogPosts/BlogPosts.tsx
+++ src/components/sections/BlogPosts/BlogPosts.tsx
@@ -40,13 +40,13 @@ const BlogPosts = () => {
 const BlogPost = ({ title, date, tags, description }: IBlogPostProps) => {
   return (
     <li>
-      <article className="flex flex-col gap-8 border-b border-gray-200 pb-8 md:flex-row md:gap-16">
+      <article className="flex flex-col gap-4 border-b border-gray-200 pb-8 md:flex-row md:gap-16">
         <section className="md:w-1/3">
           <p className="text-gray-500">{date}</p>
         </section>
-        <section className="flex flex-col gap-8 md:w-2/3">
-          <section className="flex flex-col gap-6">
-            <h6>{title}</h6>
+        <section className="flex flex-col gap-4 md:w-2/3 md:gap-8">
+          <section className="flex flex-col gap-4 md:gap-6">
+            <h5>{title}</h5>
             <BlogPostTags tags={tags} />
           </section>
           <p className="text-gray-500">{description}</p>
