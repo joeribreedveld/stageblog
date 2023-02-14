bfff0d3f977636532c6abf00090d3255a3fe9fd0
Duck1s <32653@ma-web.nl>
2023-02-14 22:21:59 +0100

    fix(footer): add more spacing bottom on mobile for better view of footer

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -3,7 +3,7 @@
 // Functions
 const Footer = () => {
   return (
-    <footer className="page-width pb-8 md:pb-0">
+    <footer className="page-width pb-16 md:pb-0">
       <p>Gemaakt door Joeri Breedveld</p>
     </footer>
   );
