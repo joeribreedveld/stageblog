4dec22887a78946e7e68d1e81bd8c55eb65d7918
Duck1s <joeri.breedveld123@gmail.com>
2023-02-15 11:49:31 +0100

    feat(information): add information variants and framer animations

--- src/components/sections/InformationList/InformationList.tsx
+++ src/components/sections/InformationList/InformationList.tsx
@@ -1,11 +1,58 @@
 // Imports
 import { IInformationListItemProps } from "./InformationList.types";
+import { motion } from "framer-motion";
+import { useMediaQuery } from "react-responsive";
+
+// Framer
+const informationListVariants = {
+  initial: {
+    opacity: 1,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.08,
+    },
+  },
+};
+
+const informationListItemVariants = {
+  initial: {
+    x: -50,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+    },
+  },
+};
+
+const noAnimationVariants = {
+  initial: {
+    opacity: 1,
+    x: 0,
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+  },
+};
 
 // Functions
 const InformationList = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+
   return (
     <section>
-      <ul className="flex flex-col gap-8">
+      <motion.ul
+        variants={isMobile ? noAnimationVariants : informationListVariants}
+        initial="initial"
+        animate="animate"
+        className="flex flex-col gap-8"
+      >
         <InformationListItem title="Naam" value="Joeri Breedveld" />
         <InformationListItem
           title="E-mailadres"
@@ -24,21 +71,27 @@ const InformationList = () => {
           title="Beschrijving"
           value="Wij ontwikkelen custom apps en platformen voor bedrijven met een specifieke vraag waar geen standaard oplossing voor bestaat. Wij ademen digital en kunnen met ons team en partners vrijwel iedere uitdaging aan. Ons hart gaat echt sneller kloppen van uitdagingen binnen domeinen als gezondheid, energie, mobiliteit, non-profit, IoT, e-learning, e-commerce, custom interfaces en automatisering."
         />
-      </ul>
+      </motion.ul>
     </section>
   );
 };
 
 const InformationListItem = ({ title, value }: IInformationListItemProps) => {
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+
   return (
-    <li className="flex flex-col gap-4 border-b border-gray-200 pb-8 md:flex-row md:gap-16">
+    <motion.li
+      variants={isMobile ? noAnimationVariants : informationListItemVariants}
+      key={title}
+      className="flex flex-col gap-4 border-b border-gray-200 pb-8 md:flex-row md:gap-16"
+    >
       <section className="md:w-1/3">
         <p className="text-gray-500">{title}</p>
       </section>
       <section className="md:w-2/3">
         <p>{value}</p>
       </section>
-    </li>
+    </motion.li>
   );
 };
 
